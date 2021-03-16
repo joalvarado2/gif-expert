59c3de2049e80e651e5f99028e027f7c2c110420
joalvarado2 <jonathanalva92@gmail.com>
2021-03-15 20:39:01 -0500

    first commit

---
+++ docs/precache-manifest.b5e6bf62063d477d26078ab8317becc1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2021f1e54e404bf2f2deb08168146966",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6db33aca8e5459758ea7",
+    "url": "/static/css/main.832776f2.chunk.css"
+  },
+  {
+    "revision": "8c639a6bff16b54b9f77",
+    "url": "/static/js/2.629c55af.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.629c55af.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6db33aca8e5459758ea7",
+    "url": "/static/js/main.df28a3b7.chunk.js"
+  },
+  {
+    "revision": "5bba4b9ed69c6568a70e",
+    "url": "/static/js/runtime-main.95118175.js"
+  }
+]);
